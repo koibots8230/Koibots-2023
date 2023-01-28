b0e058e19d28b7882a0f32b3016d3c79d6a06601
Conrad Skelly <conradskelly@gmail.com>
2023-01-28 13:12:56 -0500

    c_hanged libary to CANSparkMAx

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,13 +20,12 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public class Constants {
-   /**
-    * public static final class DriveConstants {
-    *   public static final int kLeftMotor1Port = 0;
-    *   public static final int kLeftMotor2Port = 1;
-    *   public static final int kRightMotor1Port = 2;
-    *   public static final int kRightMotor2Port = 3; 
-    * }
-    */ 
+    public static final class DriveConstants {
+       public static final int kLeftMotor1Port = 15;
+       public static final int kLeftMotor2Port = 14;
+       public static final int kRightMotor1Port = 13;
+       public static final int kRightMotor2Port = 12; 
+     }
+    
 }
 
