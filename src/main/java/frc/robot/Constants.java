7f1350cadc47ebd4ba35eb87662c39f120966cc4
Evan <evannolcott@gmail.com>
2023-02-04 10:26:01 -0500

    Add Auto Balance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,6 +25,8 @@ public class Constants {
     public static final int kRightMotor1Port = 12;
     public static final int kRightMotor2Port = 13;
     public static final int kIntakeMotorPort = 0; // To be changed when we have an actual intake
+    public static final double AUTO_SPEED = 0.15;
+
     //PID constants for PIDCommand Setmotor:
     public static final double kp = 6e-5;
     public static final double ki = 0;
