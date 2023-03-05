577dd76849e4c0c87c01af2493f025031a3e5b0a
CalebNeal07 <Caleboneal07@gmail.com>
2023-03-05 07:13:53 -0500

    New auto to leave community and balance

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,8 +38,6 @@ import com.kauailabs.navx.frc.AHRS;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem.CommunityShotCommand;
 
-
-
 /**
  * This class is where the bulk of the robot should be declared. Since
  * Command-based is a
@@ -52,14 +50,16 @@ import frc.robot.subsystems.ShooterSubsystem.CommunityShotCommand;
  */
 public class RobotContainer {
   private static RobotContainer m_robotContainer = new RobotContainer();
-  
+
   // Subsystems
   public final TankDriveSubsystem m_tankDriveSubsystem = new TankDriveSubsystem();
   public final IntakeSubsystem m_intake = new IntakeSubsystem();
 
   public final ShooterSubsystem m_ShooterSubsystem = new ShooterSubsystem();
-  //public final VisionSubsystem m_VisionSubsystem = new VisionSubsystem(m_sideChooser.getSelected());
-  private MiscDashboardSubsystem m_miscDashboardSubsystem = new MiscDashboardSubsystem(m_intake, m_ShooterSubsystem, m_tankDriveSubsystem);
+  // public final VisionSubsystem m_VisionSubsystem = new
+  // VisionSubsystem(m_sideChooser.getSelected());
+  private MiscDashboardSubsystem m_miscDashboardSubsystem = new MiscDashboardSubsystem(m_intake, m_ShooterSubsystem,
+      m_tankDriveSubsystem);
 
   // Controlers
   private final CommandXboxController m_driverHID = new CommandXboxController(0);
@@ -73,19 +73,17 @@ public class RobotContainer {
       rightDriveTrain,
       leftDriveTrain,
       m_tankDriveSubsystem);
-  
-  
+
   // Shuffleboard
   SendableChooser<Integer> m_autoChooser;
   SendableChooser<Boolean> m_customAuto;
   SendableChooser<Integer> m_customChooser;
   SendableChooser<Double> m_intakeSpeedChooser;
-  //SendableChooser<Boolean> m_sideChooser;
+  // SendableChooser<Boolean> m_sideChooser;
 
-
-  GenericEntry autobal_leftSpeed; //= m_autotab.add("autobal leftSpeed", Constants.AUTO_LEFT_SPEED).getEntry();
-  GenericEntry autobal_rightSpeed; //= m_autotab.add("autobal rightSpeed", Constants.AUTO_RIGHT_SPEED).getEntry();
-  GenericEntry shoot_leftSpeed; //= m_autotab.add("shoot leftSpeed", Constants.SHOOT_LEFT_SPEED).getEntry();
+  GenericEntry autobal_leftSpeed; // = m_autotab.add("autobal leftSpeed", Constants.AUTO_LEFT_SPEED).getEntry();
+  GenericEntry autobal_rightSpeed; // = m_autotab.add("autobal rightSpeed", Constants.AUTO_RIGHT_SPEED).getEntry();
+  GenericEntry shoot_leftSpeed; // = m_autotab.add("shoot leftSpeed", Constants.SHOOT_LEFT_SPEED).getEntry();
   GenericEntry shoot_rightSpeed;// = m_autotab.add("shoot rightSpeed", Constants.SHOOT_RIGHT_SPEED).getEntry();
   GenericEntry shoot_time; // = m_autotab.add("Shoot time", Constants.SHOOT_SECONDS).getEntry();
   GenericEntry autobal_limit;
@@ -99,8 +97,9 @@ public class RobotContainer {
 
     // Create Triggers here | Triggers should be named t_CommandName
 
-    // ======================================Operator Controls======================================
-    
+    // ======================================Operator
+    // Controls======================================
+
     // Speed Up/Down
     Trigger operatorSpeedUp = m_operatorHID.cross();
     Trigger operatorSpeedDown = m_operatorHID.circle();
@@ -109,8 +108,12 @@ public class RobotContainer {
     operatorSpeedDown.onTrue(new setSpeedCommand(false, m_tankDriveSubsystem));
 
     // LED
-    //Trigger leftTrigger_op = m_operatorHID.axisGreaterThan(PS4Controller.Axis.kL2.value, Constants.DEADZONE);
-    //Trigger rightTrigger_op = m_operatorHID.axisGreaterThan(PS4Controller.Axis.kR2.value, Constants.DEADZONE);
+    // Trigger leftTrigger_op =
+    // m_operatorHID.axisGreaterThan(PS4Controller.Axis.kL2.value,
+    // Constants.DEADZONE);
+    // Trigger rightTrigger_op =
+    // m_operatorHID.axisGreaterThan(PS4Controller.Axis.kR2.value,
+    // Constants.DEADZONE);
 
     // Shooting
     Trigger shootL2 = m_operatorHID.L1();
@@ -121,7 +124,7 @@ public class RobotContainer {
 
     // Manual Intake Up/Down
     Trigger intakeMoveUp = m_operatorHID.axisLessThan(PS4Controller.Axis.kLeftY.value, -.3);
-    Trigger intakeMoveDown = m_operatorHID.axisGreaterThan(PS4Controller.Axis.kLeftY.value, .3); 
+    Trigger intakeMoveDown = m_operatorHID.axisGreaterThan(PS4Controller.Axis.kLeftY.value, .3);
     intakeMoveUp.whileTrue(new IntakeManualCommand(m_intake, true));
     intakeMoveDown.whileTrue(new IntakeManualCommand(m_intake, false));
 
@@ -129,8 +132,8 @@ public class RobotContainer {
 
     clearButton.whileTrue(new InstantCommand(() -> m_intake.ClearStickies(), m_intake));
 
-
-    // ======================================DRIVER CONTROLS======================================
+    // ======================================DRIVER
+    // CONTROLS======================================
     // create commands
     // 5 = left bumper
     // 6 = right bumper
@@ -139,10 +142,10 @@ public class RobotContainer {
     Trigger shootTrigger = m_driverHID.axisGreaterThan(XboxController.Axis.kLeftTrigger.value, Constants.DEADZONE);
     CommunityShotCommand com_shot_cmd = m_ShooterSubsystem.new CommunityShotCommand(m_ShooterSubsystem);
     shootTrigger.whileTrue(com_shot_cmd);
-    
+
     // Flip Intake
-    //Trigger flipTrigger = m_driverHID.leftBumper();
-    //flipTrigger.onTrue(m_intake.new FlipIntake(m_intake));
+    // Trigger flipTrigger = m_driverHID.leftBumper();
+    // flipTrigger.onTrue(m_intake.new FlipIntake(m_intake));
 
     Trigger runIntakeForwardsTrigger = m_driverHID.rightTrigger(Constants.DEADZONE);
     runIntakeForwardsTrigger.whileTrue(new IntakeCommand(m_intake, true));
@@ -150,82 +153,83 @@ public class RobotContainer {
     // Reverse Intake/Midtake/Shooter
     Trigger runIntakeBackwardsTrigger = m_driverHID.rightBumper();
     runIntakeBackwardsTrigger.whileTrue(new IntakeCommand(m_intake, false)
-    .alongWith(Commands.runEnd(
-      () -> m_ShooterSubsystem.SetShooter(-.1), 
-      () -> m_ShooterSubsystem.SetShooter(0), 
-      m_ShooterSubsystem)));
-    
+        .alongWith(Commands.runEnd(
+            () -> m_ShooterSubsystem.SetShooter(-.1),
+            () -> m_ShooterSubsystem.SetShooter(0),
+            m_ShooterSubsystem)));
+
     // Slow Shooter
-    Trigger runInShooterSlowly=m_operatorHID.square();
-      runInShooterSlowly.whileTrue(new IntakeCommand(m_intake, true)
-      .alongWith(Commands.runEnd(
-        ()->m_ShooterSubsystem.SetShooter(.2), 
-        ()->m_ShooterSubsystem.SetShooter(0),
-        m_ShooterSubsystem)));
+    Trigger runInShooterSlowly = m_operatorHID.square();
+    runInShooterSlowly.whileTrue(new IntakeCommand(m_intake, true)
+        .alongWith(Commands.runEnd(
+            () -> m_ShooterSubsystem.SetShooter(.2),
+            () -> m_ShooterSubsystem.SetShooter(0),
+            m_ShooterSubsystem)));
 
   }
 
   public static RobotContainer getInstance() {
     return m_robotContainer;
   }
-    /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
-     */
-    private RobotContainer() {
-      //choosing what auto
-      m_autoChooser = new SendableChooser<Integer>();
 
-      m_autoChooser.addOption("Shoot->Move", 0);
-      m_autoChooser.addOption("Shoot->Autobalance", 1);
-      m_autoChooser.addOption("DO NOTHING", 2);
-      m_customAuto = new SendableChooser<Boolean>();
-      //integer representes which auto we get
-      m_customChooser = new SendableChooser<Integer>();
-      //chosing parameters of auto
-      ShuffleboardTab m_autotab = Shuffleboard.getTab("Auto");
-      m_customAuto.setDefaultOption("Default autos", false);
-      m_customAuto.addOption("Custom auto", true);
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  private RobotContainer() {
+    // choosing what auto
+    m_autoChooser = new SendableChooser<Integer>();
 
-      m_customChooser.addOption("Shoot->Move", 0);
-      m_customChooser.addOption("Shoot->Autobalance", 1);
+    m_autoChooser.addOption("Shoot->Move", 0);
+    m_autoChooser.addOption("Shoot->Autobalance", 1);
+    m_autoChooser.addOption("DO NOTHING", 2);
+    m_autoChooser.addOption("Exit Community + Balance", 3);
+    m_customAuto = new SendableChooser<Boolean>();
+    // integer representes which auto we get
+    m_customChooser = new SendableChooser<Integer>();
+    // chosing parameters of auto
+    ShuffleboardTab m_autotab = Shuffleboard.getTab("Auto");
+    m_customAuto.setDefaultOption("Default autos", false);
+    m_customAuto.addOption("Custom auto", true);
 
-      autobal_leftSpeed = m_autotab.add("autobal leftSpeed", Constants.AUTO_LEFT_SPEED).getEntry();
-      autobal_rightSpeed = m_autotab.add("autobal rightSpeed", Constants.AUTO_RIGHT_SPEED).getEntry();
-      shoot_leftSpeed = m_autotab.add("shoot leftSpeed", Constants.SHOOT_LEFT_SPEED).getEntry();
-      shoot_rightSpeed = m_autotab.add("shoot rightSpeed", Constants.SHOOT_RIGHT_SPEED).getEntry();
-      shoot_time = m_autotab.add("Shoot time", Constants.SHOOT_SECONDS).getEntry();
-      autobal_limit = m_autotab.add("ShootAutobalance Encoder Limit", Constants.AUTOBALANCE_MOVE_LIMIT).getEntry();
-      shoot_limit= m_autotab.add("ShootMove EncoderLimit", Constants.SHOOT_MOVE_LIMIT).getEntry();
+    m_customChooser.addOption("Shoot->Move", 0);
+    m_customChooser.addOption("Shoot->Autobalance", 1);
 
+    autobal_leftSpeed = m_autotab.add("autobal leftSpeed", Constants.AUTO_LEFT_SPEED).getEntry();
+    autobal_rightSpeed = m_autotab.add("autobal rightSpeed", Constants.AUTO_RIGHT_SPEED).getEntry();
+    shoot_leftSpeed = m_autotab.add("shoot leftSpeed", Constants.SHOOT_LEFT_SPEED).getEntry();
+    shoot_rightSpeed = m_autotab.add("shoot rightSpeed", Constants.SHOOT_RIGHT_SPEED).getEntry();
+    shoot_time = m_autotab.add("Shoot time", Constants.SHOOT_SECONDS).getEntry();
+    autobal_limit = m_autotab.add("ShootAutobalance Encoder Limit", Constants.AUTOBALANCE_MOVE_LIMIT).getEntry();
+    shoot_limit = m_autotab.add("ShootMove EncoderLimit", Constants.SHOOT_MOVE_LIMIT).getEntry();
 
-      AHRS m_gyro = new AHRS(SPI.Port.kMXP);
-      // m_autoChooser.setDefaultOption("Shoot->Move", new shootMove(m_tankDriveSubsystem, m_ShooterSubsystem, m_intake,
-      // Constants.SHOOT_SECONDS, 
-      // Constants.SHOOT_MOVE_LIMIT, 
-      // Constants.SHOOT_LEFT_SPEED, 
-      // Constants.SHOOT_RIGHT_SPEED));
-      // m_autoChooser.addOption("Shoot->Autobalance", new shootAutobalance(
-      //   m_tankDriveSubsystem,
-      //   m_ShooterSubsystem, 
-      //   Constants.SHOOT_SECONDS, 
-      //   Constants.AUTOBALANCE_MOVE_LIMIT,
-      //   Constants.AUTO_LEFT_SPEED, 
-      //   Constants.AUTO_RIGHT_SPEED,
-      //   m_gyro,
-      //   m_intake
-      // ));
+    AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+    // m_autoChooser.setDefaultOption("Shoot->Move", new
+    // shootMove(m_tankDriveSubsystem, m_ShooterSubsystem, m_intake,
+    // Constants.SHOOT_SECONDS,
+    // Constants.SHOOT_MOVE_LIMIT,
+    // Constants.SHOOT_LEFT_SPEED,
+    // Constants.SHOOT_RIGHT_SPEED));
+    // m_autoChooser.addOption("Shoot->Autobalance", new shootAutobalance(
+    // m_tankDriveSubsystem,
+    // m_ShooterSubsystem,
+    // Constants.SHOOT_SECONDS,
+    // Constants.AUTOBALANCE_MOVE_LIMIT,
+    // Constants.AUTO_LEFT_SPEED,
+    // Constants.AUTO_RIGHT_SPEED,
+    // m_gyro,
+    // m_intake
+    // ));
 
-      m_autoChooser.addOption(("NO AUTO"), null);
-        configureButtonBindings();
-      ShuffleboardTab m_shuffleboard = Shuffleboard.getTab("Main");
-      m_shuffleboard.add(m_autoChooser);
-      m_autotab.add(m_customAuto);
-      m_autotab.add(m_autoChooser);
-      m_shuffleboard.addNumber("Encoder Left", ()->m_tankDriveSubsystem.getEncoderPositions()[0]);
-      m_shuffleboard.addNumber("Encoder Right", ()->m_tankDriveSubsystem.getEncoderPositions()[1]);
-      
+    m_autoChooser.addOption(("NO AUTO"), null);
+    configureButtonBindings();
+    ShuffleboardTab m_shuffleboard = Shuffleboard.getTab("Main");
+    m_shuffleboard.add(m_autoChooser);
+    m_autotab.add(m_customAuto);
+    m_autotab.add(m_autoChooser);
+    m_shuffleboard.addNumber("Encoder Left", () -> m_tankDriveSubsystem.getEncoderPositions()[0]);
+    m_shuffleboard.addNumber("Encoder Right", () -> m_tankDriveSubsystem.getEncoderPositions()[1]);
 
-    }
+  }
 
   public CommandGenericHID getController() {
     return m_driverHID;
@@ -234,60 +238,73 @@ public class RobotContainer {
   public void ResetPositions() {
     m_intake.resetPosition();
   }
+
   public TankDriveSubsystem getDrive() {
     return m_tankDriveSubsystem;
   }
+
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    //check if custom auto
-    //by defualt is set to false
+    // check if custom auto
+    // by defualt is set to false
     if (m_customAuto.getSelected()) {
-      
-      switch(m_customChooser.getSelected()){
-        //break statements unecessary due to return function
-        case(0):
+
+      switch (m_customChooser.getSelected()) {
+        // break statements unecessary due to return function
+        case (0):
           return new shootMove(m_tankDriveSubsystem, m_ShooterSubsystem, m_intake,
-          Constants.SHOOT_SECONDS, 
-          Constants.SHOOT_MOVE_LIMIT, 
-          Constants.SHOOT_LEFT_SPEED, 
-          Constants.SHOOT_RIGHT_SPEED);
-        case(1):
+              Constants.SHOOT_SECONDS,
+              Constants.SHOOT_MOVE_LIMIT,
+              Constants.SHOOT_LEFT_SPEED,
+              Constants.SHOOT_RIGHT_SPEED);
+        case (1):
           AHRS m_gyro = new AHRS(SPI.Port.kMXP);
-          return new shootAutobalance(m_tankDriveSubsystem, m_ShooterSubsystem, 
-          Constants.SHOOT_SECONDS, 
-          Constants.AUTOBALANCE_MOVE_LIMIT,
-          Constants.AUTO_LEFT_SPEED, 
-          Constants.AUTO_RIGHT_SPEED,
-          m_gyro,
-          m_intake);
-      } 
-      //if nothing seletcted
+          return new shootAutobalance(m_tankDriveSubsystem, m_ShooterSubsystem,
+              Constants.SHOOT_SECONDS,
+              Constants.AUTOBALANCE_MOVE_LIMIT,
+              Constants.AUTO_LEFT_SPEED,
+              Constants.AUTO_RIGHT_SPEED,
+              m_gyro,
+              m_intake);
+      }
+      // if nothing seletcted
       return null;
     } else {
-    switch(m_autoChooser.getSelected()){
-      case(0):
-      return new shootMove(m_tankDriveSubsystem, m_ShooterSubsystem, m_intake,
-      Constants.SHOOT_SECONDS, 
-      Constants.SHOOT_MOVE_LIMIT, 
-      Constants.SHOOT_LEFT_SPEED, 
-      Constants.SHOOT_RIGHT_SPEED);
-      case(1):
-      AHRS m_gyro = new AHRS(SPI.Port.kMXP);
-      return new shootAutobalance(m_tankDriveSubsystem, m_ShooterSubsystem, 
-      Constants.SHOOT_SECONDS, 
-      Constants.AUTOBALANCE_MOVE_LIMIT,
-      Constants.AUTO_LEFT_SPEED, 
-      Constants.AUTO_RIGHT_SPEED,
-      m_gyro,
-      m_intake);
-      case(2):
+      switch (m_autoChooser.getSelected()) {
+        case (0):
+          return new shootMove(m_tankDriveSubsystem, m_ShooterSubsystem, m_intake,
+              Constants.SHOOT_SECONDS,
+              Constants.SHOOT_MOVE_LIMIT,
+              Constants.SHOOT_LEFT_SPEED,
+              Constants.SHOOT_RIGHT_SPEED);
+        case (1):
+          AHRS m_gyro = new AHRS(SPI.Port.kMXP);
+          return new shootAutobalance(m_tankDriveSubsystem, m_ShooterSubsystem,
+              Constants.SHOOT_SECONDS,
+              Constants.AUTOBALANCE_MOVE_LIMIT,
+              Constants.AUTO_LEFT_SPEED,
+              Constants.AUTO_RIGHT_SPEED,
+              m_gyro,
+              m_intake);
+        case (2):
+          return null;
+        case (3):
+          AHRS n_gyro = new AHRS(SPI.Port.kMXP);
+          return new CommunityBalance(
+              m_tankDriveSubsystem,
+              m_ShooterSubsystem,
+              Constants.SHOOT_SECONDS,
+              Constants.AUTOBALANCE_MOVE_LIMIT,
+              Constants.AUTO_LEFT_SPEED,
+              Constants.AUTO_RIGHT_SPEED,
+              n_gyro,
+              m_intake);
+      }
       return null;
-    }
-    return null;
     }
   }
 }
