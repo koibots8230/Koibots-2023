561faa49554159a6537ad375b91cef3a65f115c7
Akshay Kuchibhatla <akshaykuchibhatla@gmail.com>
2023-04-07 22:33:53 -0400

    Moved shooter values back to same speeds

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,24 +16,28 @@ public class ShooterSubsystem extends SubsystemBase {
   private CANSparkMax shooterMotorL;
   private CANSparkMax shooterMotorR;
 
-  private RelativeEncoder shooterEncoder;
+  private RelativeEncoder leftShooterEncoder;
+  private RelativeEncoder rightShootEncoder;
 
   ShooterSubsystem() {
     shooterMotorL = new CANSparkMax(Constants.SHOOTER_MOTOR_L, MotorType.kBrushless);
     shooterMotorR = new CANSparkMax(Constants.SHOOTER_MOTOR_R, MotorType.kBrushless);
     shooterMotorL.setInverted(true);
 
-    shooterEncoder = shooterMotorL.getEncoder();
+    leftShooterEncoder = shooterMotorL.getEncoder();
+    rightShootEncoder = shooterMotorR.getEncoder();
 
     shooterMotorR.setInverted(false);
   }
 
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Left Speed", leftShooterEncoder.getVelocity());
+    SmartDashboard.putNumber("Right Speed", rightShootEncoder.getVelocity());
+  }
+
   public static ShooterSubsystem get() {
     return m_ShooterSubsystem;
-  }
-   
-  public double getShooterSpeed() {
-    return shooterEncoder.getVelocity();
   }
 
   public void SetShooter(double lSpeed, double rSpeed) {
@@ -70,26 +75,26 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public Command CommunityShot() {
-    return new Shoot(Constants.COMMUNITY_SHOOTER_SPEED, Constants.COMMUNITY_SHOOTER_SPEED);
+    return new Shoot(Constants.COMMUNITY_SHOOTER_SPEED);
   }
 
   public Command L1Shot() {
-    return new Shoot(Constants.L1_SHOOTER_SPEED, Constants.L1_SHOOTER_SPEED*1.5);
+    return new Shoot(Constants.L1_SHOOTER_SPEED);
   }
 
   public Command StationToHybridShot() {
-    return new Shoot(0.55, 0.55*1.5);
+    return new Shoot(0.6);
   }
 
   public Command AutoL2Shot() {
-    return new Shoot(Constants.AUTO_L2_SHOOTER_SPEED, Constants.AUTO_L2_SHOOTER_SPEED*1.5);
+    return new Shoot(Constants.AUTO_L2_SHOOTER_SPEED);
   }
   
   public Command L2Shot() {  
-    return new Shoot(Constants.L2_SHOOTER_SPEED, Constants.L2_SHOOTER_SPEED*1.5);
+    return new Shoot(Constants.L2_SHOOTER_SPEED);
   }
 
   public Command HybriShot() {
-    return new Shoot(Constants.HYBRID_SHOOTER_SPEED, Constants.HYBRID_SHOOTER_SPEED*1.5);
+    return new Shoot(Constants.HYBRID_SHOOTER_SPEED);
   }
 }
